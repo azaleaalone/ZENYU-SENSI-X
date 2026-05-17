d5b07a285741401b8e20922a3b1d9554422962cc
azaleaalone <girlstown111@gmail.com>
2026-05-17 09:11:17 +0545

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by zenyu_alone",
+        name: "Made by zenyu",
         // Activity type number (0 = Playing).
         type: 0, 
       },
